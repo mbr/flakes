69e06e112202fac8cd90b94d5fa42804052f9ffb
Marc Brinkmann <git@marcbrinkmann.de>
2025-11-26 22:46:41 +0100

    Style elm app a little

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
@@ -37,11 +38,28 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text ("Count: " ++ String.fromInt model.count) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , button [ onClick Reset ] [ text "Reset" ]
+    div [ class "flex flex-col items-center justify-center min-h-screen bg-gray-100" ]
+        [ div [ class "bg-white rounded-lg shadow-lg p-8 space-y-6" ]
+            [ div [ class "text-6xl font-bold text-center text-gray-800" ]
+                [ text (String.fromInt model.count) ]
+            , div [ class "flex gap-3 justify-center" ]
+                [ button
+                    [ onClick Decrement
+                    , class "px-6 py-3 bg-red-500 hover:bg-red-600 text-white rounded-lg font-semibold transition-colors"
+                    ]
+                    [ text "-" ]
+                , button
+                    [ onClick Increment
+                    , class "px-6 py-3 bg-green-500 hover:bg-green-600 text-white rounded-lg font-semibold transition-colors"
+                    ]
+                    [ text "+" ]
+                , button
+                    [ onClick Reset
+                    , class "px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white rounded-lg font-semibold transition-colors"
+                    ]
+                    [ text "Reset" ]
+                ]
+            ]
         ]
 
 
