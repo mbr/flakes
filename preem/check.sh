d264cca3518a5b4db703d11b3fe8c8b3348a213d
Marc Brinkmann <git@marcbrinkmann.de>
2026-08-12 16:46:44 +0200

    Simplify PREEM checks

--- preem/check.sh
+++ preem/check.sh
@@ -4,17 +4,6 @@ set -eu
 cd "$(dirname "$0")"
 
 ./format.sh --check
-
-printf 'rustc %s at %s, cargo %s at %s\n' \
-  "$(rustc --version)" \
-  "$(which rustc)" \
-  "$(cargo --version)" \
-  "$(which cargo)"
-
-RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-D warnings" \
-  cargo test --manifest-path backend/Cargo.toml
-RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-D warnings" \
-  cargo check --manifest-path backend/Cargo.toml
-cargo clippy --manifest-path backend/Cargo.toml -- -D warnings
-
+cargo test --manifest-path backend/Cargo.toml
+cargo clippy --manifest-path backend/Cargo.toml --all-targets -- -D warnings
 ./frontend/build.sh
