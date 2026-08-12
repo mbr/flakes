1c553a0e3670c419d3ceb01aacf59bd0ffcb0932
Marc Brinkmann <git@marcbrinkmann.de>
2026-08-12 16:55:03 +0200

    Simplify backend formatting script

--- preem/backend/format.sh
+++ preem/backend/format.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
-set -eu
+
+#: Formats the backend source.
+#: Uses --config to override rustfmt settings without a nightly toolchain.
+#: As a little hack, supports `--check`.
 
 cd "$(dirname "$0")"
 
-case "${1:-}" in
-  --check)
-    cargo fmt -- \
-      --config group_imports=StdExternalCrate \
-      --config imports_granularity=Crate \
-      --check
-    nixfmt --check package.nix
-    ;;
-  "")
-    cargo fmt -- \
-      --config group_imports=StdExternalCrate \
-      --config imports_granularity=Crate
-    nixfmt package.nix
-    ;;
-  *)
-    echo "usage: ./format.sh [--check]" >&2
-    exit 2
-    ;;
-esac
+cargo fmt -- --config group_imports=StdExternalCrate --config imports_granularity=Crate "$@"
+nixfmt "$@" package.nix
