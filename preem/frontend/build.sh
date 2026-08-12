6215d3fb11049930b2ba36a61441c308f232b859
Marc Brinkmann <git@marcbrinkmann.de>
2026-08-12 21:02:36 +0200

    Simplify frontend build flags

--- preem/frontend/build.sh
+++ preem/frontend/build.sh
@@ -7,22 +7,22 @@ rm -rf dist
 mkdir -p dist
 cp -R public/. dist/
 
-build() {
-  elm make src/Main.elm "$1" --output=elm-stuff/app.js
-  shift
-  esbuild elm-stuff/app.js --outfile=dist/app.js --log-level=warning "$@"
-  tailwindcss -i css/input.css -o dist/app.css "$@"
-}
-
 case "${1:-}" in
   --release)
-    build --optimize --minify
+    elm_flag=--optimize
+    minify_flag=--minify
     ;;
   "")
-    build --debug
+    elm_flag=--debug
+    minify_flag=
     ;;
   *)
     echo "usage: ./build.sh [--release]" >&2
     exit 2
     ;;
 esac
+
+elm make src/Main.elm "$elm_flag" --output=elm-stuff/app.js
+# Reprint Elm's JavaScript so Tailwind can recognize escaped class names.
+esbuild elm-stuff/app.js --outfile=dist/app.js --log-level=warning $minify_flag
+tailwindcss -i css/input.css -o dist/app.css $minify_flag
