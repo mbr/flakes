8a66f2e7f11055435d8c87a0e402eabba70fee42
Marc Brinkmann <git@marcbrinkmann.de>
2026-08-12 21:15:56 +0200

    Delegate formatter argument validation

--- preem/format.sh
+++ preem/format.sh
@@ -3,11 +3,6 @@ set -eu
 
 cd "$(dirname "$0")"
 
-case "${1:-}" in
-  --check | "") ;;
-  *) echo "usage: ./format.sh [--check]" >&2; exit 2 ;;
-esac
-
 nixfmt "$@" flake.nix nixos-module.nix
 ./backend/format.sh "$@"
 ./frontend/format.sh "$@"
