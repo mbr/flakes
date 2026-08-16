1524fc06bbf4875c4a1423760b0114bd9f359d45
Marc Brinkmann <git@marcbrinkmann.de>
2026-08-16 12:12:25 +0200

    Simplify Elm dependency updates

--- preem/frontend/update-elm-deps.sh
+++ preem/frontend/update-elm-deps.sh
@@ -6,16 +6,6 @@ set -eu
 
 cd "$(dirname "$0")"
 
-temporary_directory="$(mktemp -d)"
-trap 'rm -rf "$temporary_directory"' EXIT HUP INT TERM
-
-cp elm.json "$temporary_directory/"
-(
-  cd "$temporary_directory"
-  elm2nix convert > elm-srcs.nix
-  elm2nix snapshot
-)
-
-mv "$temporary_directory/elm-srcs.nix" elm-srcs.nix
-mv "$temporary_directory/registry.dat" registry.dat
+elm2nix convert > elm-srcs.nix
+elm2nix snapshot --write-to registry.dat
 nix fmt elm-srcs.nix
